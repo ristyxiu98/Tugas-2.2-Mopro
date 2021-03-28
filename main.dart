0e0470080e9050ab2644c8058e60c68f37c957db
ristyxiu98 <70812499+ristyxiu98@users.noreply.github.com>
2021-03-28 10:51:30 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 120,
+                height: 120,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(image: AssetImage("images/gbr.png")),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Adinda Risty',
+                style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Text(
+                'Mahasiswa',
+                style: TextStyle(
+                  color: Colors.amberAccent,
+                  fontSize: 18,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 25, top: 15),
+                width: MediaQuery.of(context).size.width * 0.1,
+                height: 1,
+                color: Colors.white70,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 24),
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 14),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.call,
+                      color: Colors.grey,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 24,
+                    ),
+                    Text(
+                      '+6289651408873',
+                      style: TextStyle(fontSize: 16, letterSpacing: 1),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 24),
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 14),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.grey,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 24,
+                    ),
+                    Text(
+                      'adindaristy123@gmail.com',
+                      style: TextStyle(fontSize: 16, letterSpacing: 1),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
